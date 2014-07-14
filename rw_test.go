3e141ec349d7e9226ad6248615a4ac16619740dd
Packrat386 <packrat386@gmail.com>
2014-07-14 15:47:48 -0400

    fixed some errors with the tests

--- rw_test.go
+++ rw_test.go
@@ -14,7 +14,7 @@ func TestBasicWriteRead(t *testing.T) {
 		Body: &body,
 	}
 	c := newtestclient("http://localhost:8098")
-	err := c.CreateObject(obj)
+	err := c.CreateObject(obj, nil)
 	if err != nil {
 		t.Errorf("Recieved error: %s", err)
 		body, _ := httputil.DumpRequest(c.lastreq(), false)
@@ -37,3 +37,43 @@ func TestBasicWriteRead(t *testing.T) {
 	}
 }
 
+func TestClockConflict(t *testing.T) {
+	var bodyA bytes.Buffer
+	bodyA.WriteString("Testing, 1, 2, 3")
+	objA := &Object {
+		Key: "testKey",
+		Bucket: "testing",
+		Body: &bodyA,
+	}
+
+	var bodyB bytes.Buffer
+	bodyB.WriteString("Testing, 1, 2, 3")
+	objB := &Object {
+		Key: "testKey",
+		Bucket: "testing",
+		Body: &bodyB,
+	}
+
+	c := newtestclient("http://localhost:8098")
+	err := c.Store(objA, nil)
+	if err != nil { 
+		t.Errorf("Recieved error: %s", err)
+		body, _ := httputil.DumpRequest(c.lastreq(), false)
+		t.Errorf("Last request: %s", body)
+		res, _ := httputil.DumpResponse(c.lastres(), false)
+		t.Fatalf("Last response: %s", res)
+	}
+	err = c.Store(objB, nil)
+	if err != nil { 
+		t.Errorf("Recieved error: %s", err)
+		body, _ := httputil.DumpRequest(c.lastreq(), false)
+		t.Errorf("Last request: %s", body)
+		res, _ := httputil.DumpResponse(c.lastres(), false)
+		t.Fatalf("Last response: %s", res)
+	}
+	
+	_, err = c.Fetch(objB.path(), nil)
+	if _, ok := err.(*ErrMultipleVclocks); !ok {
+		t.Errorf("Expected ErrMultipleVclocks, instead receieved error: %s", err)
+	}
+}
