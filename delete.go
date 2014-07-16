c26ddf99678ec48fc1f4dc17b21f390efc55ac0e
Packrat386 <packrat386@gmail.com>
2014-07-16 16:47:19 -0400

    added delete.go shell

---
+++ delete.go
@@ -0,0 +1,12 @@
+package riak
+
+import (
+	"net/http"
+	"net/url"
+)
+
+// Delete removes the specified object from the database
+func (c *Client) Delete(o *Object, opts map[string]string) error {
+	// TODO: this whole function
+	return nil
+}
